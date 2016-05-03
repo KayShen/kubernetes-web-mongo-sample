015617bccc862ff1e64122b5041aa947c76b5435
Jivko Vantchev <jivko.vantchev@egym.de>
2016-05-03 17:34:23 +0200

    Removes unnecessary backup scripts and adds the PDF
    Removes the backups scripts that were used for the creation of our
    backup presentation solution.
    
    Adds the presentaiton PDF in the meetup/presentation folder.

--- meetup/backup/b1-namespace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf 'Creating the guestbook namespace...\n'
-
-printf 'kubectl create -f k8s/guestbook.namespace.yaml\n'
-kubectl create -f k8s/guestbook.namespace.yaml
-
-printf 'kubectl get namespace\n'
-kubectl get namespace
